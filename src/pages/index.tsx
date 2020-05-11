f793cf1e829b8544b2a26b15913fe319a845ade3
Shihab Chowdhury <sachow168@gmail.com>
2020-05-11 19:29:11 -0400

    Added SCSS functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-function IndexPage() {
-    return <div>Hello World Next - Typescript - Express</div>;
+import '../styles/index.scss';
+const IndexPage = () => {
+    return <div className="main">This is a boilerplate using Next.JS, Express, TypeScript, and SCSS to create a web application.</div>;
 }
   
 export default IndexPage;
