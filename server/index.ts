6036ac1da16cde187dcbda01a1c9aad246677efc
Shihab Chowdhury <sachow168@gmail.com>
2020-05-11 12:57:32 -0400

    Initial project structure

---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+    server.get('/hello', async (req, res) => {
+        return res.json({ hello: 'world' });
+    })
+    server.get('/', (req, res) => {
+        return app.render(req, res, '/');
+    })
+    server.all("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
