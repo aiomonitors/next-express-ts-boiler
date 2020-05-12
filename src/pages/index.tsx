644a03c626ba99078329b44cc4801625a2f1f849
Shihab Chowdhury <sachow168@gmail.com>
2020-05-11 20:38:52 -0400

    Added tslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import '../styles/index.scss';
 const IndexPage = () => {
-    return <div className="main">This is a boilerplate using Next.JS, Express, TypeScript, and SCSS to create a web application.</div>;
+    return ( <div className="main">
+        This is a boilerplate using Next.JS, Express, TypeScript, and SCSS to create a web application.
+        <br />
+        Created by <a href="https://www.shihab.dev">Shihab Chowdhury</a>
+        </div>
+    );
 }
   
 export default IndexPage;
